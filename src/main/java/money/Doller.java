f6f639fdc8ac992672feacf788d34cd62da3abc1
HirokiNakano <baseballinobu@gmail.com>
2026-03-29 12:12:47 +0900

    doller method moves to parent class Money

--- src/main/java/money/Doller.java
+++ src/main/java/money/Doller.java
@@ -1,17 +1,10 @@
 package money;
 
-public class Doller {
-    private int amount;
-
+public class Doller extends Money {
     Doller(int amount) {
         this.amount = amount;
     }
     Doller times(int multiplier) {
         return new Doller(this.amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Doller doller = (Doller) object;
-        return this.amount == doller.amount;
     }
 }
