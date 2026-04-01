6ee7479ff87a416bf724c7243254ed1b24252017
HirokiNakano <baseballinobu@gmail.com>
2026-04-01 20:10:46 +0900

    moneyの抽象化、処理の共通化

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -1,10 +1,10 @@
 package money;
 
 public class Franc extends Money{
-    Franc(int amount) {
-        this.amount = amount;
+    Franc(int amount, String currency) {
+        super(amount, currency);
     }
-    Franc times(int multiplier) {
-        return new Franc(this.amount * multiplier);
+    Money times(int multiplier) {
+        return Money.franc(amount * multiplier);
     }
 }
