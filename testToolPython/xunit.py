26854d31fd8e03078c8ec156c58d687384f542b3
HirokiNakano <baseballinobu@gmail.com>
2026-04-07 23:14:23 +0900

    tearDown

--- testToolPython/xunit.py
+++ testToolPython/xunit.py
@@ -1,22 +1,28 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
+    def setUp(self):
+        self.log = "setUp"
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + " testMethod"
+    def tearDown(self):
+        self.log = self.log + " tearDown"
 
 class TestCaseTest(TestCase):
-    def testRunning(self):
+    def testTemplateMethod(self):
         test = WasRun("testMethod")
-        assert(not test.wasRun)
         test.run()
-        assert(test.wasRun)
-        
-TestCaseTest("testRunning").run()
+        assert("setUp testMethod tearDown" == test.log)
+
+TestCaseTest("testTemplateMethod").run()
