129f087a222cbeffcbb897d77bcbd70c7b437ebd
HirokiNakano <baseballinobu@gmail.com>
2026-04-08 07:05:09 +0900

    testResult

--- testToolPython/xunit.py
+++ testToolPython/xunit.py
@@ -1,3 +1,14 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def testFaild(self):
+        self.errorCount = self.errorCount + 1
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -6,16 +17,24 @@ class TestCase:
     def tearDown(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFaild()
         self.tearDown()
+        return result
 
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp"
     def testMethod(self):
         self.log = self.log + " testMethod"
+    def testBrokenMethod(self):
+        raise Exception
     def tearDown(self):
         self.log = self.log + " tearDown"
 
@@ -24,5 +43,21 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown" == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFaild()
+        assert("1 run, 1 failed" == result.summary())
 
-TestCaseTest("testTemplateMethod").run()
+print(TestCaseTest("testTemplateMethod").run().summary())
+print(TestCaseTest("testResult").run().summary())
+print(TestCaseTest("testFailedResult").run().summary())
+print(TestCaseTest("testFailedResultFormatting").run().summary())
